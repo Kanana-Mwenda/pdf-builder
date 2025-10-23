51201f7425bc9fe7167e2e7558af10fa28908165
Kanana-Mwenda <Lizamwenda95@gmail.com>
2025-10-23 12:33:43 +0300

    added spacing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
-import java.io.*;
-
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class Main {
     public static void main(String[] args) {
+        String htmlFilePath = ""
+
+
         try {
             // Output pdf code...
 
