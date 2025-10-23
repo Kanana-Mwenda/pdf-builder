ba11622817d96df1a20eaa96be0b4d56756444bd
Kanana-Mwenda <Lizamwenda95@gmail.com>
2025-10-23 14:16:44 +0300

    added java code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,29 @@
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.nio.file.Files;
-import java.nio.file.Paths;
 
 
 public class Main {
     public static void main(String[] args) {
-        String htmlFilePath = ""
-
+        String htmlFilePath = "src/main/resources/p-nine-report.html";
+        String outputPdfPath = "src/out/report.pdf";
 
         try {
-            // Output pdf code...
+            new File("src/out").mkdirs();
 
+            try(FileOutputStream os= new FileOutputStream(outputPdfPath)) {
+                PdfRendererBuilder builder = new PdfRendererBuilder(); 
+                builder.useFastMode();
+                builder.withFile(new File(htmlFilePath));
+                builder.toStream(os);
+                builder.run();
+            }
+
+            System.out.println("PDF created successfully at: " + outputPdfPath);
 
         } catch (Exception e) {
             e.printStackTrace();
+            System.err.println("Error generating PDF: " + e.getMessage());
         }
     }
 }
